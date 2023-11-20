5a9b877d9e2cf2e166052b85c3b6a9c5f68c0f06
sophiayuanproj <sophia38yuan@gmail.com>
2023-11-19 16:00:16 -0800

    Created login page

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div className = "login">
+      <div className = "content">
+        <center>
+        <h1>Welcome back!</h1>
+        </center>
+        <Login />
+      </div>
     </div>
   );
 }
 
-export default App;
+function Login(){
+  return(
+    <div>
+      <center>
+        <form>
+        <label>
+          Email:
+        <input type = "text" name = "email"/>
+        <br></br>
+        <br></br>
+          Password:
+        <input type = "text" name = "password"/>
+        <br></br>
+        <br></br>
+        </label>
+        <SigninButton></SigninButton>
+      </form>
+      </center>
+    </div>
+  );
+}
+
+function SigninButton() {
+  return(
+    <div>
+    <button type = "button">Sign in</button>
+    </div>
+
+  );
+
+}
